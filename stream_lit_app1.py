0bc21d9d9e07a0689a2ed82f071fc98d11f65c2c
ashishSF123 <96582958+ashishSF123@users.noreply.github.com>
2024-11-22 22:36:29 +0530

    Update stream_lit_app1.py

--- stream_lit_app1.py
+++ stream_lit_app1.py
@@ -46,5 +46,13 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+if ingredients_list:
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
+
