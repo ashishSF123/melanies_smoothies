5437238465eccefa13c24c99229ace905d3f1c48
ashishSF123 <96582958+ashishSF123@users.noreply.github.com>
2024-11-22 23:49:47 +0530

    Update stream_lit_app1.py

--- stream_lit_app1.py
+++ stream_lit_app1.py
@@ -59,12 +59,12 @@ if ingredients_list:
 
 
     search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-    st.write('The search value for ', fruit_chosen,' is ', search_on, '.')    
+    #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')    
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition_Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
