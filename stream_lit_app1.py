3d62e2f9adbeea10b8d6c8454042d9a682fcb32d
ashishSF123 <96582958+ashishSF123@users.noreply.github.com>
2024-11-22 20:18:24 +0530

    Update stream_lit_app1.py

--- stream_lit_app1.py
+++ stream_lit_app1.py
@@ -1,13 +1,24 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col
+from snowflake.snowpark.functions import col, when_matched
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom smoothie.
     """
 )
+
+# Establish Snowflake connection and session
+cnx = st.connection(
+    "snowflake",
+    account="GPCZETT.CGB39045",
+    user="ashish",
+    password="@rjuN1997",
+    role="SYSADMIN",
+    warehouse="COMPUTE_WH",
+    database="smoothies",
+    schema="public")
+session = cnx.session()
 
 
 
